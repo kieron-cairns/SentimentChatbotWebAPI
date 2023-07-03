56489ef64bd9929f1a22bc4d5a5d63b1f558b0f3
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-03 13:05:00 +0200

    CHANGE: Mock http message handler added for unit testing purproses

---
+++ SentimentChatbotWebAPITests/MockObjects/MockHttpMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentChatbotWebAPITests.MockObjects
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunc;
+
+        public MockHttpMessageHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handlerFunc)
+        {
+            _handlerFunc = handlerFunc;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return _handlerFunc(request, cancellationToken);
+        }
+    }
+}
