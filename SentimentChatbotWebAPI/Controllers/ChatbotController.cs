c8e0a89fc620ca1115d6d4463dd9e49a7d98c12d
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-03 12:39:06 +0200

    CHANGE: Default controller and class added, new controller file added

---
+++ SentimentChatbotWebAPI/Controllers/ChatbotController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SentimentChatbotWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatbotController : ControllerBase
+    {
+    }
+}
