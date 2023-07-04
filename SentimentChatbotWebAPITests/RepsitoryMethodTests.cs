c6250e44b4d06ba939429fbf0fa12937eedf6214
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-04 11:33:34 +0200

    CHANGE: Repository method unit test class file added and constructor implemented

---
+++ SentimentChatbotWebAPITests/RepsitoryMethodTests.cs
@@ -0,0 +1,34 @@
+﻿using Castle.Core.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Moq;
+using SentimentChatbotWebAPI.Interfaces;
+using SentimentChatbotWebAPI.Models;
+using SentimentChatbotWebAPI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentChatbotWebAPITests
+{
+    public class RepsitoryMethodTests
+    {
+        private readonly Mock<IJwtTokenHandler> _jwtTokenHandlerMock;
+        private readonly Mock<IAzureSecretClientWrapper> _azureSecretClientWrapperMock;
+        private readonly Mock<IConfiguration> _configurationMock;
+        private readonly User _user;
+        private readonly ChatbotRepository _chatbotRepository;
+        private SecurityToken _fakeToken;
+
+
+        public RepsitoryMethodTests()
+        {
+            _jwtTokenHandlerMock = new Mock<IJwtTokenHandler>();
+            _azureSecretClientWrapperMock = new Mock<IAzureSecretClientWrapper>();
+            _configurationMock = new Mock<IConfiguration>();
+        }
+    }
+
+    
+}
