ad18cab4aa6647ce821f76ed3a5a98a941e95c6a
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-04 11:15:01 +0200

    CHANGE: Jwt Token Handler Wrapper class file implemented

--- SentimentChatbotWebAPI/Interfaces/IJwtTokenHandler.cs
+++ SentimentChatbotWebAPI/Interfaces/IJwtTokenHandler.cs
@@ -1,6 +1,11 @@
-﻿namespace SentimentChatbotWebAPI.Interfaces
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace SentimentChatbotWebAPI.Interfaces
 {
     public interface IJwtTokenHandler
     {
+        string WriteToken(SecurityToken token);
+        ClaimsPrincipal ValidateToken(string token, TokenValidationParameters validationParameters, out SecurityToken validatedToken);
     }
 }
---
+++ SentimentChatbotWebAPI/Utilities/JwtTokenHandlerWrapper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SentimentChatbotWebAPI.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace SentimentChatbotWebAPI.Utilities
+{
+    public class JwtTokenHandlerWrapper : IJwtTokenHandler
+    {
+        private readonly JwtSecurityTokenHandler _tokenHandler;
+
+        public JwtTokenHandlerWrapper()
+        {
+            _tokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public string WriteToken(SecurityToken token)
+        {
+            return _tokenHandler.WriteToken(token);
+        }
+
+        public ClaimsPrincipal ValidateToken(string token, TokenValidationParameters validationParameters, out SecurityToken validatedToken)
+        {
+            return _tokenHandler.ValidateToken(token, validationParameters, out validatedToken);
+        }
+    }
+}
