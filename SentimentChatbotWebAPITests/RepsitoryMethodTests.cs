5a5df9767f7c00c387db9398bc4f83fada02b107
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-04 11:38:31 +0200

    CHANGE: Unit test passes for successful JWT token generation

--- SentimentChatbotWebAPITests/RepsitoryMethodTests.cs
+++ SentimentChatbotWebAPITests/RepsitoryMethodTests.cs
@@ -1,12 +1,14 @@
-﻿using Castle.Core.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using Moq;
+using SentimentChatbotWebAPI.Data;
 using SentimentChatbotWebAPI.Interfaces;
 using SentimentChatbotWebAPI.Models;
 using SentimentChatbotWebAPI.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,6 +29,33 @@ namespace SentimentChatbotWebAPITests
             _jwtTokenHandlerMock = new Mock<IJwtTokenHandler>();
             _azureSecretClientWrapperMock = new Mock<IAzureSecretClientWrapper>();
             _configurationMock = new Mock<IConfiguration>();
+            _user = new User { Username = "testUsername", Password = "TestPassword", Role = "testRole" };
+
+        }
+
+        [Fact]
+        public void Generate_JWT_Token_Succesfully_Creates_Token()
+        {
+            var mockContext = new Mock<ISentimentHistoryContext>();
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSecretKey"));
+            _jwtTokenHandlerMock.Setup(th => th.WriteToken(It.IsAny<SecurityToken>())).Returns("fakeTokenString");
+            _jwtTokenHandlerMock.Setup(th => th.ValidateToken(It.IsAny<string>(), It.IsAny<TokenValidationParameters>(), out _fakeToken))
+                .Returns(new ClaimsPrincipal(new ClaimsIdentity(new[] {
+        new Claim(ClaimTypes.NameIdentifier, _user.Username),
+        new Claim(ClaimTypes.Role, _user.Role)
+                })));
+
+            _azureSecretClientWrapperMock.Setup(sPM => sPM.GetSecret("JWT-Secret-Token")).Returns("YourSecretKey");
+
+            var repository = new ChatbotRepository(mockContext.Object, _configurationMock.Object, _jwtTokenHandlerMock.Object, _azureSecretClientWrapperMock.Object);
+
+            var tokenString = repository.GenerateJwtToken(_user);
+
+            // Assert that tokenString is correct
+            Assert.Equal("fakeTokenString", tokenString);
+
+            // Now you can make further assertions based on what your method is supposed to do.
         }
     }
 
