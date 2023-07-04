4a31e508fdf750bdd64b83adf66340097da0f29a
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-04 11:27:04 +0200

    CHANGE: Reposioty method added for JWT token generation

--- SentimentChatbotWebAPI/Repository/ChatbotRepository.cs
+++ SentimentChatbotWebAPI/Repository/ChatbotRepository.cs
@@ -1,4 +1,9 @@
-﻿using SentimentChatbotWebAPI.Interfaces;
+﻿using Microsoft.IdentityModel.Tokens;
+using SentimentChatbotWebAPI.Interfaces;
+using SentimentChatbotWebAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SentimentChatbotWebAPI.Repository
 {
@@ -17,5 +22,42 @@ namespace SentimentChatbotWebAPI.Repository
             _jwtTokenHandler = jwtTokenHandler;
             _azureSecretClientWrapper = azureSecretClientWrapper;
         }
+
+        public string GenerateJwtToken(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_azureSecretClientWrapper.GetSecret("JWT-Secret-Token")));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Username),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+            claims,
+                expires: DateTime.UtcNow.AddSeconds(30),
+                signingCredentials: credentials);
+
+            var tokenString = _jwtTokenHandler.WriteToken(token);
+
+
+            // Configure token validation parameters
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+                ValidateIssuer = true,
+                ValidIssuer = _configuration["Jwt:Issuer"],
+                ValidateAudience = true,
+                ValidAudience = _configuration["Jwt:Audience"],
+                ValidateLifetime = true // Enable validation of the token's lifetime
+            };
+
+            // Validate the token
+            var principal = _jwtTokenHandler.ValidateToken(tokenString, validationParameters, out var validatedToken);
+
+            return tokenString;
+        }
+
     }
 }
