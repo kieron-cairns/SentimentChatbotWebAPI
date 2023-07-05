c8a50b46d3735676696a5e64dd947173e70c39c2
kieron-kmc-ltd <kieron@kmc-technologies.ltd>
2023-07-05 13:25:57 +0200

    CHANGE: Program.cs class file modified to account for web application factory for integration testing purposes

--- SentimentChatbotWebAPI/Program.cs
+++ SentimentChatbotWebAPI/Program.cs
@@ -9,86 +9,102 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using SentimentChatbotWebAPI.Repository;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-// Retrieve the SQL connection string from Azure Key Vault
-var keyVaultUrl = new Uri(builder.Configuration.GetSection("keyVaultConfig:KVUrl").Value!);
-var keyVaultTenantId = builder.Configuration.GetSection("keyVaultConfig:TenantId").Value;
-var keyVaultClientId = builder.Configuration.GetSection("keyVaultConfig:ClientId").Value;
-var keyVaultSecret = builder.Configuration.GetSection("keyVaultConfig:ClientSecretId").Value;
-
-var azureCredential = new ClientSecretCredential(keyVaultTenantId, keyVaultClientId, keyVaultSecret);
-
-var client = new SecretClient(keyVaultUrl, azureCredential);
-
-var connectionString = client.GetSecret(builder.Configuration.GetSection("ConnectionStrings:Test-DB").Value).Value.Value;
-
-// Add the DbContext with the SQL connection string to the service container
-builder.Services.AddDbContext<SentimentQueryHistoryContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
-builder.Services.AddHttpClient();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<IChatbotRepository, ChatbotRepository>();
-builder.Services.AddScoped<ISentimentQueryHistoryContext, SentimentQueryHistoryContext>();
-builder.Services.AddScoped<IJwtTokenHandler, JwtTokenHandlerWrapper>();
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<IAzureKeyVaultWrapper>(sp =>
+public class Program
 {
-    var config = sp.GetRequiredService<IConfiguration>();
-    string keyVaultUrl = config["keyVaultConfig:KVUrl"];
-    string tenantId = config["keyVaultConfig:TenantId"];
-    string clientId = config["keyVaultConfig:ClientId"];
-    string clientSecretId = config["keyVaultConfig:ClientSecretId"];
-
-    var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecretId);
-    var secretClient = new SecretClient(new Uri(keyVaultUrl), clientSecretCredential);
-    return new AzureKeyVaultWrapper(secretClient);
-});
-
-builder.Services.AddSingleton<IAzureSecretClientWrapper, AzureSecretClientWrapper>();
-
-var jwtSecretToken = client.GetSecret(builder.Configuration.GetSection("JWTConfig:TokenName").Value).Value.Value;
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
-    options.TokenValidationParameters = new TokenValidationParameters
+    public static void Main(string[] args)
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretToken))
-    };
-});
+        BuildApplication(args).Run();
+    }
 
-var app = builder.Build();
+    public static WebApplication BuildApplication(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Your existing setup code...
+        // Add services to the container.
+
+
+        // Retrieve the SQL connection string from Azure Key Vault
+        var keyVaultUrl = new Uri(builder.Configuration.GetSection("keyVaultConfig:KVUrl").Value!);
+        var keyVaultTenantId = builder.Configuration.GetSection("keyVaultConfig:TenantId").Value;
+        var keyVaultClientId = builder.Configuration.GetSection("keyVaultConfig:ClientId").Value;
+        var keyVaultSecret = builder.Configuration.GetSection("keyVaultConfig:ClientSecretId").Value;
+        // Retrieve the SQL connection string from Azure Key Vault
+        // ...rest of your existing code
+
+
+        var azureCredential = new ClientSecretCredential(keyVaultTenantId, keyVaultClientId, keyVaultSecret);
+
+        var client = new SecretClient(keyVaultUrl, azureCredential);
+
+        var connectionString = client.GetSecret(builder.Configuration.GetSection("ConnectionStrings:Test-DB").Value).Value.Value;
+
+        // Add the DbContext with the SQL connection string to the service container
+        builder.Services.AddDbContext<SentimentQueryHistoryContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+        builder.Services.AddHttpClient();
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<IChatbotRepository, ChatbotRepository>();
+        builder.Services.AddScoped<ISentimentQueryHistoryContext, SentimentQueryHistoryContext>();
+        builder.Services.AddScoped<IJwtTokenHandler, JwtTokenHandlerWrapper>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddSingleton<IAzureKeyVaultWrapper>(sp =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            string keyVaultUrl = config["keyVaultConfig:KVUrl"];
+            string tenantId = config["keyVaultConfig:TenantId"];
+            string clientId = config["keyVaultConfig:ClientId"];
+            string clientSecretId = config["keyVaultConfig:ClientSecretId"];
+
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecretId);
+            var secretClient = new SecretClient(new Uri(keyVaultUrl), clientSecretCredential);
+            return new AzureKeyVaultWrapper(secretClient);
+        });
+
+        builder.Services.AddSingleton<IAzureSecretClientWrapper, AzureSecretClientWrapper>();
+
+        var jwtSecretToken = client.GetSecret(builder.Configuration.GetSection("JWTConfig:TokenName").Value).Value.Value;
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecretToken))
+            };
+        });
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthentication();
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
 }
-
-app.UseAuthentication();
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
--- SentimentChatbotWebAPITests/IntegrationTests.cs
+++ SentimentChatbotWebAPITests/IntegrationTests.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SentimentChatbotWebAPITests
 {
-    public class IntegrationTests
-    {
 
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>> // replace 'YourNamespace' with the actual namespace of your Program class
+    {
+        private readonly WebApplicationFactory<Program> _factory; // replace 'YourNamespace' with the actual namespace of your Program class
+
+        public IntegrationTests(WebApplicationFactory<Program> factory) // replace 'YourNamespace' with the actual namespace of your Program class
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task VerifyBearer_AuthorizedUser_ReturnsOk()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Simulate Authentication by setting up Bearer token
+            // This should be replaced by a function that generates a valid token
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "Your valid JWT token here");
+
+            // Act
+            var response = await client.GetAsync("/VerifyBearer");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
     }
+
+
 }
